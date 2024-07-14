a93f17f9bf37206bf9cd0619f65ec6b2671e5c98
Srijan Bista <srijanbista4284@gmail.com>
2024-07-14 16:15:46 +0545

    created login form and login page.

---
+++ src/app/components/LoginForm.tsx
@@ -0,0 +1,12 @@
+const LoginForm = () => {
+  return (
+    <div className="border flex flex-col items-center justify-center py-10 px-5">
+      <h1 className="text-slate-800 font-extrabold text-5xl">
+        Welcome to Promage!
+      </h1>
+      <span className="text-lg">Sign Up to Continue</span>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm";
+
+const Login = () => {
+  return (
+    <div className="h-screen w-screen bg-white grid place-items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
