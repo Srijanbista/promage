d56c1a4bde8edb31be0953020988c042a2a07778
Srijan Bista <srijanbista4284@gmail.com>
2024-07-14 16:23:23 +0545

    added sidenav

--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -1,5 +1,11 @@
+"use client";
 const SideNav = () => {
-  return <div>SideNav</div>;
+  const token = localStorage.getItem("user:token");
+  const handleLogOut = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+  return <div>{token && <button onClick={handleLogOut}>Log Out</button>}</div>;
 };
 
 export default SideNav;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthenticationProvider from "./components/Authentication";
+import SideNav from "./components/SideNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthenticationProvider>{children}</AuthenticationProvider>
+        <AuthenticationProvider>
+          <SideNav />
+          {children}
+        </AuthenticationProvider>
       </body>
     </html>
   );
