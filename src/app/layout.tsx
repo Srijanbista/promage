e9a6d0af2e868b3e7f2d8c78f5d1300747864534
Srijan Bista <srijanbista4284@gmail.com>
2024-07-15 11:55:26 +0545

    home page progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthenticationProvider from "./components/Authentication";
-import SideNav from "./components/SideNav";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthenticationProvider>
-          <SideNav />
-          {children}
-        </AuthenticationProvider>
+        <Toaster />
+        <AuthenticationProvider>{children}</AuthenticationProvider>
       </body>
     </html>
   );
