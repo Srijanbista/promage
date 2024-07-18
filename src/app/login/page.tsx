8dcfc86d3de78d1d78446b4821e6dfb3b2aaa9d0
Srijan Bista <srijanbista4284@gmail.com>
2024-07-18 10:52:38 +0545

    added loaderwith backdrop

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+import { useState } from "react";
 import LoginForm from "../components/LoginForm";
+import LoaderWithBackdrop from "../components/LoaderWithBackdrop";
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="h-screen w-screen bg-primary grid place-items-center">
-      <LoginForm />
+      <LoaderWithBackdrop isLoading={isLoading} />
+      <LoginForm setIsLoading={setIsLoading} />
     </div>
   );
 };
