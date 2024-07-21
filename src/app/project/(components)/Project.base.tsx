501092e2dfd01b5e8d96e8dfb7b7cb24bb566869
Srijan Bista <srijanbista4284@gmail.com>
2024-07-21 19:34:13 +0545

    ui:showed two columns after md breapoint

--- src/app/project/(components)/Project.base.tsx
+++ src/app/project/(components)/Project.base.tsx
@@ -12,7 +12,7 @@ export const ProjectBase = ({
   const [isLoading, setIsLoading] = useState(false);
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {projects.map((project) => (
           <ProjectCard
             key={project.id}
